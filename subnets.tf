e7c5aaf5ac7536e6d01b522ef3bfcad8cb4bb4a1
unknown <anilkumarravuri510@gmail.com>
2023-05-04 01:52:57 +0530

    auto push

--- subnets.tf
+++ subnets.tf
@@ -1,3 +1,4 @@
+#CREATES PUBLIC SUBNETS
 resource "aws_subnet" "public_subnet" {
   count              = length(var.PUBLIC_SUBNET_CIDR)
   vpc_id             = aws_vpc.main.id
@@ -6,5 +7,18 @@ resource "aws_subnet" "public_subnet" {
 
   tags = {
     Name = "robot-public-subnet-${element(var.AZ, count.index)}"
+  }
+}
+
+
+#CREATES PRIVATE SUBNETS
+resource "aws_subnet" "private_subnet" {
+  count              = length(var.PRIVATE_SUBNET_CIDR)
+  vpc_id             = aws_vpc.main.id
+  cidr_block         = element(var.PRIVATE_SUBNET_CIDR, count.index)
+  availability_zone  = element(var.AZ, count.index)
+
+  tags = {
+    Name = "robot-private-subnet-${element(var.AZ, count.index)}"
   }
 }
