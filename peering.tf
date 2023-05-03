1b9800cbd186cbdbcc1ebd81304c7b894cca1da8
unknown <anilkumarravuri510@gmail.com>
2023-05-04 02:12:31 +0530

    auto push

---
+++ peering.tf
@@ -0,0 +1,5 @@
+resource "aws_vpc_peering_connection" "peer" {
+// peer_owner_id = var.peer_owner_id  # if your peer vpc which are in the same account , this optin is not needed
+  peer_vpc_id   = aws_vpc.bar.id
+  vpc_id        = "var.DEFAULT_VPC_ID"
+}
