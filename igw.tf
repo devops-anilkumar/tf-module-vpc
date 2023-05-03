cb20968e30b412fda25fb8e373f1fc89e7c83e8c
unknown <anilkumarravuri510@gmail.com>
2023-05-04 03:12:00 +0530

    auto push

--- igw.tf
+++ igw.tf
@@ -6,25 +6,3 @@ resource "aws_internet_gateway" "igw" {
     Name = "roboshop-${var.ENV}-igw"
   }
 }
-#ELASTIC IP NEEDED FOR NAT GATEWAY
-resource "aws_eip" "ngw-eip" {
-  vpc      = true
-  tags = {
-    name = "roboshop-${var.ENV}-ngw-eip"
-  }
-}
-
-
-# CREATES NAT GATEWAY TO EXPOSE THE PRIVATE NETWORK TO PRIVATE MACHINES
-resource "aws_nat_gateway" "ngw" {
-  allocation_id = aws_eip.ngw-eip.id
-  subnet_id     = aws_subnet.public_subnet.*.id[0]
-
-  tags = {
-    Name = "robot-${var.ENV}-ngw"
-  }
-
-  # To ensure proper ordering, it is recommended to add an explicit dependency
-  # on the Internet Gateway for the VPC.
-  depends_on = [aws_eip.ngw-eip]
-}
---
+++ ngw.tf
@@ -0,0 +1,22 @@
+#ELASTIC IP NEEDED FOR NAT GATEWAY
+resource "aws_eip" "ngw-eip" {
+  vpc      = true
+  tags = {
+    name = "roboshop-${var.ENV}-ngw-eip"
+  }
+}
+
+
+# CREATES NAT GATEWAY TO EXPOSE THE PRIVATE NETWORK TO PRIVATE MACHINES
+resource "aws_nat_gateway" "ngw" {
+  allocation_id = aws_eip.ngw-eip.id
+  subnet_id     = aws_subnet.public_subnet.*.id[0]
+
+  tags = {
+    Name = "robot-${var.ENV}-ngw"
+  }
+
+  # To ensure proper ordering, it is recommended to add an explicit dependency
+  # on the Internet Gateway for the VPC.
+  depends_on = [aws_eip.ngw-eip]
+}
