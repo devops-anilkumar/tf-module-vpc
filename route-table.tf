8ef32a3ce182feb43b53977a14bcfee283c40aa9
unknown <anilkumarravuri510@gmail.com>
2023-05-04 23:56:46 +0530

    auto push

--- route-table.tf
+++ route-table.tf
@@ -46,3 +46,11 @@ resource "aws_route_table_association" "private-rt-association" {
   subnet_id               = element(aws_subnet.private_subnet.*.id, count.index)
   route_table_id          = aws_route_table.private-rt.id
 }
+
+# ADDING THE PEERING ROUTE ENTRY INSIDE THE DEFAULT ROUTE TABLE TO ACESS PROJECT ROBOT NETWORK
+resource "aws_route" "default-vpc-robot-vpc-root" {
+  route_table_id            = var.DEFAULT_VPC_RT
+  destination_cidr_block    = var.VPC_CIDR
+  vpc_peering_connection_id = aws_vpc_peering_connection.peer.id
+  depends_on                = [aws_vpc_peering_connection.peer]
+} 
