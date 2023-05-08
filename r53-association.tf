354b2d37e3eab3345907078faa86ba2533579f01
unknown <anilkumarravuri510@gmail.com>
2023-05-08 11:33:01 +0530

    auto push

--- r53-association.tf
+++ r53-association.tf
@@ -1,4 +1,4 @@
 resource "aws_route53_zone_association" "secondary" {
-  zone_id = "Z01019823KGEEYKYEE9GQ" // var.PRIVATE_HOSTED_ZONE_ID
+  zone_id = var.PRIVATE_HOSTED_ZONE_ID
   vpc_id  = aws_vpc.main.id
 }
